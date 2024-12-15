c2fc47f246a8cbaa0497da656e07a68d8db83469
pratik <pratikhanmante3@gmail.com>
2024-12-15 20:16:11 +0530

    -

---
+++ src/test/java/io/pratik/contactApi/ContactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pratik.contactApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
